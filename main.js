dbccdd89eb89b9bd3c5ee2d3abe227ac889f7107
ls-ron <rony24th@gmail.com>
2024-12-04 10:37:55 +1300

    basic functionality added to html/css/js

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 const container = document.getElementById('container');
-const childContainers = document.querySelectorAll('.child-container');
 
-container.addEventListener('mousemove', (e) => {
-  const rect = container.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
+const fragment = document.createDocumentFragment();
+let number = 100 * 100
+for (let i = 0; i < number; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('child-container');
+    fragment.appendChild(newDiv);
+}
 
-  const childIndex = Math.floor(x / 100) + Math.floor(y / 100) * Math.floor(container.offsetWidth / 100);
-  
-  childContainers.forEach((child, index) => {
-    child.classList.remove('pixelated');
-  });
+container.appendChild(fragment);
 
-  if (childIndex >= 0 && childIndex < childContainers.length) {
-    childContainers[childIndex].classList.add('pixelated');
-    
+const childContainer = document.querySelectorAll('.child-container');
+
+function handleMouseEnter(event) {
+    event.target.classList.add('pixelated');
+}
+
+function handleMouseLeave(event) {
     setTimeout(() => {
-      childContainers[childIndex].classList.remove('pixelated');
+    event.target.classList.remove('pixelated');
     }, 200);
-  }
+}
+
+childContainer.forEach(item => {
+    item.addEventListener('mouseenter', handleMouseEnter);
+    item.addEventListener('mouseleave', handleMouseLeave);
 });
